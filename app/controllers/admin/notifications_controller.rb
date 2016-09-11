4814c3a253fd00d7aa0c642524b4a5ad63ad58e3
Luiz Sanches <luizgrsanches@gmail.com>
2016-09-11 19:59:14 -0300

    fixed verify CSRF

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -1,4 +1,6 @@
 class Admin::NotificationsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :create
+
   def create
     transaction = PagSeguro::Transaction.find_by_notification_code(params[:notificationCode])
 
