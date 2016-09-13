b21179e57828c0f0e6f44c8e2010e04ff69f2286
Luiz Sanches <luizgrsanches@gmail.com>
2016-09-13 14:47:50 -0300

    set return url

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -22,9 +22,9 @@ class CreditsController < ApplicationController
     payment = PagSeguro::PaymentRequest.new
 
     payment.reference = "CC#{@credit.id}"
-    payment.notification_url = ENV['PAGSEGURO_NOTIFICATION_URL']
-    payment.redirect_url = "#{request.base_url}/credits"
-    payment.abandon_url = "#{request.base_url}/credits"
+    payment.notification_url = "#{ENV['RETURN_URL']}/admin/notifications"
+    payment.redirect_url = "#{ENV['RETURN_URL']}/credits"
+    payment.abandon_url = "#{ENV['RETURN_URL']}/credits"
 
     payment.items << {
       id: '1',
