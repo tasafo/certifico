a3fd95c2a5d7ae9024b133f56152279bf04ad1b1
Luiz Sanches <luizsanches@opmbx.org>
2017-06-15 12:46:42 -0300

    fixed notifications

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -1,5 +1,5 @@
 class Admin::NotificationsController < ApplicationController
-  skip_before_filter :verify_authenticity_token, only: :create
+  skip_before_action :verify_authenticity_token, only: :create
 
   def create
     notification_code = params[:notificationCode]
