ad0bb6f10c8ffec5ef7be14988831b4925bcc75b
Luiz Sanches <luizsanches@opmbx.org>
2017-11-28 17:22:37 -0300

    user can be an administrator

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,7 @@ class User
 
   field :current_credits,   type: Integer, default: 5
   field :name_changed,      type: Boolean, default: false
+  field :admin,             type: Boolean, default: false
 
   slug :user_name
 
