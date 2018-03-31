8ecb35a486335a0ddf16409732f64920372c1191
Luiz Sanches <lsanches@protonmail.com>
2018-03-30 23:02:55 -0300

    fix undefined local variable

--- app/models/concerns/image_certificate.rb
+++ app/models/concerns/image_certificate.rb
@@ -14,14 +14,14 @@ class ImageCertificate
 
     image_file = ENV['CLOUDINARY_URL'].nil? ? "#{Rails.root}/public/#{url}" : "#{Rails.root}/tmp/#{certificate.id}"
 
-    download_image(image_file) unless ENV['CLOUDINARY_URL'].nil?
+    download_image(url, image_file) unless ENV['CLOUDINARY_URL'].nil?
 
     image_file
   end
 
   private
 
-  def download_image(image_file)
+  def download_image(url, image_file)
     address = 'res.cloudinary.com'
 
     image_path = url.sub("http://#{address}", "")
