6cd75ccbb38cf5dfff19dae67e288e90ee58a5fc
Luiz Sanches <lsanches@protonmail.com>
2022-02-18 17:53:40 -0300

    Fix text spaces in certificate

--- app/models/concerns/certificate_generator.rb
+++ app/models/concerns/certificate_generator.rb
@@ -20,16 +20,13 @@ class CertificateGenerator
 
   def pdf
     Prawn::Document.new(@pdf_options) do |pdf|
-      pdf.fill_color certificate.template.font_color.gsub('#', '')
+      pdf.fill_color certificate.template.font_color.delete('#')
 
       pdf.move_down 200
-      pdf.text "<font size='20'>#{certificate_body}</font>",
-               inline_format: true, align: :justify
+      pdf.text "<font size='20'>#{certificate_body}</font>", inline_format: true, align: :justify
 
-      pdf.bounding_box([pdf.bounds.right - 60, pdf.bounds.bottom - 30],
-                       width: 250, height: 20, align: :right) do
-        pdf.text "<font size='10'><link href='#{@link}'>#{@subscriber.id}</link></font>",
-                 inline_format: true
+      pdf.bounding_box([pdf.bounds.right - 60, pdf.bounds.bottom - 30], width: 250, height: 20, align: :right) do
+        pdf.text "<font size='10'><link href='#{@link}'>#{@subscriber.id}</link></font>", inline_format: true
       end
     end
   end
@@ -37,13 +34,7 @@ class CertificateGenerator
   def certificate_body
     category = certificate.category
 
-    "#{I18n.t('title.certificate.certify_that')} <b>\
-    #{subscriber.user.full_name}</b> #{I18n.t('title.certificate.attended')} \
-    #{category.preposition} #{category.name} \
-    #{certificate.title}, #{I18n.t('title.certificate.realized')} \
-    #{certificate_long_date}, #{certificate.local}, #{certificate_work_load}, \
-    #{I18n.t('title.certificate.quality_of')} <b>#{subscriber.profile.name}</b>\
-    #{certificate_theme}"
+    "#{I18n.t('title.certificate.certify_that')} <b>#{subscriber.user.full_name}</b> #{I18n.t('title.certificate.attended')} #{category.preposition} #{category.name} #{certificate.title}, #{I18n.t('title.certificate.realized')} #{certificate_long_date}, #{certificate.local}, #{certificate_work_load}, #{I18n.t('title.certificate.quality_of')} <b>#{subscriber.profile.name}</b>#{certificate_theme}"
   end
 
   def certificate_long_date
@@ -57,16 +48,11 @@ class CertificateGenerator
     if initial_date == final_date
       "#{title_date('on')} #{format_date(initial_date, :long)}"
     elsif initial_date.year != final_date.year
-      "#{title_date_between} #{format_date(initial_date, :long)} \
-      #{title_date_and} #{format_date(final_date, :long)}"
+      "#{title_date_between} #{format_date(initial_date, :long)} #{title_date_and} #{format_date(final_date, :long)}"
     elsif initial_date.month == final_date.month
-      "#{title_date_between} #{initial_date.day} #{title_date_and} \
-      #{final_date.day} #{title_date_of} \
-      #{format_date(initial_date, date_format)}"
+      "#{title_date_between} #{initial_date.day} #{title_date_and} #{final_date.day} #{title_date_of} #{format_date(initial_date, date_format)}"
     else
-      "#{title_date_between} #{initial_date.day} #{title_date_of} \
-      #{format_date(initial_date, '%B')} #{title_date_and} #{final_date.day} \
-      #{title_date_of} #{format_date(final_date, date_format)}"
+      "#{title_date_between} #{initial_date.day} #{title_date_of} #{format_date(initial_date, '%B')} #{title_date_and} #{final_date.day} #{title_date_of} #{format_date(final_date, date_format)}"
     end
   end
 
@@ -81,8 +67,7 @@ class CertificateGenerator
   def certificate_work_load
     workload = certificate.workload
 
-    "#{I18n.t('title.certificate.with_hours')} #{workload} \
-    #{I18n.t('title.certificate.workload.hour').pluralize(workload.to_i)}"
+    "#{I18n.t('title.certificate.with_hours')} #{workload} #{I18n.t('title.certificate.workload.hour').pluralize(workload.to_i)}"
   end
 
   def certificate_theme
